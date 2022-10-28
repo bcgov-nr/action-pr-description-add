d1241f7293a75463965a1ee8b3d01ffa55188b7b
Derek Roberts <derek.roberts@gmail.com>
2022-10-28 12:03:19 -0700

    Boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,45 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {getInput} from '@actions/core'
+import {context, getOctokit} from '@actions/github'
+import dedent from 'dedent'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+type GithubContext = typeof context
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const inputName = getInput('name')
+const ghToken = getInput('ghToken')
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+greet(inputName, getRepoUrl(context))
+
+getDiff().then(files => {
+  console.log(
+    dedent(`
+    Your PR diff:
+    ${JSON.stringify(files, undefined, 2)}
+    `)
+  )
+})
+
+function greet(name: string, repoUrl: string) {
+  console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`)
 }
 
-run()
+function getRepoUrl({repo, serverUrl}: GithubContext): string {
+  return `${serverUrl}/${repo.owner}/${repo.repo}`
+}
+
+async function getDiff() {
+  if (ghToken && context.payload.pull_request) {
+    const octokit = getOctokit(ghToken)
+
+    const result = await octokit.rest.repos.compareCommits({
+      repo: context.repo.repo,
+      owner: context.repo.owner,
+      head: context.payload.pull_request.head.sha,
+      base: context.payload.pull_request.base.sha,
+      per_page: 100
+    })
+
+    return result.data.files || []
+  }
+
+  return []
+}
