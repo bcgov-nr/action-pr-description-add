e1c0cd5d22f3d018dc4ae1a436858ba909f4f996
Derek Roberts <derek.roberts@gmail.com>
2022-10-28 20:35:43 -0700

    Use API to read PR description

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
-/* eslint-disable no-console */
-import * as core from '@actions/core'
+import {getInput, info} from '@actions/core'
+import {getOctokit} from '@actions/github'
 
-async function run(): Promise<void> {
-  const body: string = core.getInput('body')
-  const token: string = core.getInput('token')
-  if (!body || !token) {
-    throw new Error('Error: incomplete input!')
-  }
-  console.log('token is present')
-  console.log('body: ${body}')
+// Action input
+const append = getInput('append')
+const owner = getInput('owner')
+const pr_no = getInput('pr_no')
+const repo = getInput('repo')
+const token = getInput('token')
+
+// Validate input
+if (!append || !owner || !pr_no || !repo || !token) {
+  throw new Error('Error: incomplete input!')
 }
 
+// Authenticate
+const octokit = getOctokit(token)
+
+// Main function
+async function run(): Promise<void> {
+  const res = await octokit.request(
+    `GET /repos/${owner}/${repo}/pulls/${pr_no}`
+  )
+  info(`append: ${append}`)
+  info(`description: ${JSON.stringify(res.data.body)}`)
+}
+
+// Run main function
 run()
