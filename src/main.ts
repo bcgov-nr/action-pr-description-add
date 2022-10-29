940ede17859f73120bbbea15251b1ec4bc9e61a3
Derek Roberts <derek.roberts@gmail.com>
2022-10-28 20:21:29 -0700

    Super simple action

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,14 @@
-import {getInput} from '@actions/core'
-import {context, getOctokit} from '@actions/github'
-import dedent from 'dedent'
+/* eslint-disable no-console */
+import * as core from '@actions/core'
 
-type GithubContext = typeof context
-
-const inputName = getInput('name')
-const ghToken = getInput('ghToken')
-
-greet(inputName, getRepoUrl(context))
-
-getDiff().then(files => {
-  console.log(
-    dedent(`
-    Your PR diff:
-    ${JSON.stringify(files, undefined, 2)}
-    `)
-  )
-})
-
-function greet(name: string, repoUrl: string) {
-  console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`)
-}
-
-function getRepoUrl({repo, serverUrl}: GithubContext): string {
-  return `${serverUrl}/${repo.owner}/${repo.repo}`
-}
-
-async function getDiff() {
-  if (ghToken && context.payload.pull_request) {
-    const octokit = getOctokit(ghToken)
-
-    const result = await octokit.rest.repos.compareCommits({
-      repo: context.repo.repo,
-      owner: context.repo.owner,
-      head: context.payload.pull_request.head.sha,
-      base: context.payload.pull_request.base.sha,
-      per_page: 100
-    })
-
-    return result.data.files || []
+async function run(): Promise<void> {
+  const body: string = core.getInput('body')
+  const token: string = core.getInput('token')
+  if (!body || !token) {
+    throw new Error('Error: incomplete input!')
   }
-
-  return []
+  console.log('token is present')
+  console.log('body: ${body}')
 }
+
+run()
