003332fe539288ab92f256130528c1546f01077d
Derek Roberts <derek.roberts@gmail.com>
2023-07-04 21:27:03 -0700

    Test

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,31 @@ async function action(): Promise<void> {
   // Authenticate Octokit client
   const octokit = getOctokit(token)
 
-  // API path built from context, current PR description
-  const apiPath = `/repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.number}`
-  const description = (await octokit.request(`GET ${apiPath}`)).data.body
-
-  // Check the description for our markdown message
-  if (description.includes(markdown)) {
-    info('Markdown message is already present')
-    return
-  }
-
-  // Append markdown and update/patch description
-  info('Description is being updated')
-  await octokit.request(`PATCH ${apiPath}`, {
-    body: description.concat(`\n\n${markdown}`)
+  const {data: pullRequest} = await octokit.rest.pulls.get({
+    owner: 'bcgov-nr',
+    repo: 'action-pr-description-add',
+    pull_number: 189,
+    mediaType: {
+      format: 'diff'
+    }
   })
+  info(pullRequest)
+
+  // // API path built from context, current PR description
+  // const apiPath = `/repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.number}`
+  // const description = (await octokit.request(`GET ${apiPath}`)).data.body
+
+  // // Check the description for our markdown message
+  // if (description.includes(markdown)) {
+  //   info('Markdown message is already present')
+  //   return
+  // }
+
+  // // Append markdown and update/patch description
+  // info('Description is being updated')
+  // await octokit.request(`PATCH ${apiPath}`, {
+  //   body: description.concat(`\n\n${markdown}`)
+  // })
 }
 
 // Run main function
