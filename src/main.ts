45560d80f289c56d87694c2762014c9c8f19f732
Derek Roberts <derek.roberts@gmail.com>
2023-07-10 22:53:35 -0700

    feat: run only on opened or reopened (#195)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {context, getOctokit} from '@actions/github'
 // Action input
 const markdown = getInput('add_markdown')
 const token = getInput('github_token')
-if (!markdown || !token) {
+const limit_to_pr_opened = getInput('limit_to_pr_opened')
+
+if (!markdown || !token || !limit_to_pr_opened) {
   error('Error: please verify input!')
 }
 
 // Main function
 async function action(): Promise<void> {
+  // If limit_to_pr_opened is true, then verify status (opened, reopened)
+  const trigger = JSON.stringify(context.payload.action) || ''
+  const statuses = ['opened', 'reopened']
+  if (limit_to_pr_opened === 'true' && statuses.includes(trigger)) {
+    info('PR not opened or reopened with limit_to_pr_opened=true.  Exiting.')
+    return
+  }
+
   // Authenticate Octokit client
   const octokit = getOctokit(token)
 
@@ -23,12 +33,12 @@ async function action(): Promise<void> {
   // Exit/return if our markdown message is already present
   const body = pullRequest.body || ''
   if (body.includes(markdown)) {
-    info('Markdown message is already present')
+    info('Markdown message is already present.  Exiting.')
     return
   }
 
   // If we're here update the body
-  info('Description is being updated')
+  info('Description is being updated.')
   await octokit.rest.pulls.update({
     owner: context.repo.owner,
     repo: context.repo.repo,
