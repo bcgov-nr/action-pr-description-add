3b67c671150c1d3d967f2a8c6769b8c654460520
Derek Roberts <derek.roberts@gmail.com>
2023-09-15 18:17:07 -0700

    bug: duplicates (#240)

--- dist/index.js
+++ dist/index.js
@@ -9657,22 +9657,32 @@ function action() {
             repo: github_1.context.repo.repo,
             pull_number: github_1.context.payload.number
         });
+        // Note: Any of these checks can work
+        //   body.includes(markdown)
+        //   body.endsWith(markdown)
+        //   body.match(new RegExp(markdown))
+        //   !~body.indexOf(markdown)
+        //   !~body.search(markdown)
         // Exit/return if our markdown message is already present
-        let body = pullRequest.body || '';
-        if (body.includes(markdown)) {
+        const body = pullRequest.body || '';
+        if (body.endsWith(markdown)) {
             (0, core_1.info)('Markdown message is already present.  Exiting.');
             return;
         }
-        // There have been issues with duplicate messages, so remove those
-        body = body.split(markdown)[0];
         // If we're here update the body
-        (0, core_1.info)('Description is being updated.');
-        yield octokit.rest.pulls.update({
-            owner: github_1.context.repo.owner,
-            repo: github_1.context.repo.repo,
-            pull_number: github_1.context.payload.number,
-            body: body.concat(`\n\n${markdown}`)
-        });
+        if (!body.endsWith(markdown)) {
+            (0, core_1.info)('Description is being updated.');
+            yield octokit.rest.pulls.update({
+                owner: github_1.context.repo.owner,
+                repo: github_1.context.repo.repo,
+                pull_number: github_1.context.payload.number,
+                // Split out any duplicate messages, has been an issue
+                body: body.split(markdown)[0].concat(`\n\n${markdown}`)
+            });
+            return;
+        }
+        // If here, something went wrong ...which seems to happen a lot
+        (0, core_1.error)('Unexpected result.  Please verify the action has performed correctly.');
     });
 }
 // Run main function
--- src/main.ts
+++ src/main.ts
@@ -30,24 +30,35 @@ async function action(): Promise<void> {
     pull_number: context.payload.number
   })
 
+  // Note: Any of these checks can work
+  //   body.includes(markdown)
+  //   body.endsWith(markdown)
+  //   body.match(new RegExp(markdown))
+  //   !~body.indexOf(markdown)
+  //   !~body.search(markdown)
+
   // Exit/return if our markdown message is already present
-  let body = pullRequest.body || ''
-  if (body.includes(markdown)) {
+  const body = pullRequest.body || ''
+  if (body.endsWith(markdown)) {
     info('Markdown message is already present.  Exiting.')
     return
   }
 
-  // There have been issues with duplicate messages, so remove those
-  body = body.split(markdown)[0]
-
   // If we're here update the body
-  info('Description is being updated.')
-  await octokit.rest.pulls.update({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: context.payload.number,
-    body: body.concat(`\n\n${markdown}`)
-  })
+  if (!body.endsWith(markdown)) {
+    info('Description is being updated.')
+    await octokit.rest.pulls.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.number,
+      // Split out any duplicate messages, has been an issue
+      body: body.split(markdown)[0].concat(`\n\n${markdown}`)
+    })
+    return
+  }
+
+  // If here, something went wrong ...which seems to happen a lot
+  error('Unexpected result.  Please verify the action has performed correctly.')
 }
 
 // Run main function
